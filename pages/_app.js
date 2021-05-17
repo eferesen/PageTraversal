21ff0a32ff47b96dc2cd4316f155b548bd7371b6
Ahsan Imam <aimam@bills.com>
2021-05-17 14:35:21 -0700

    CX-4246 | ✨ Add framer motion and Layout to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+import { Layout } from "../components/layout/";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Layout>
+        <Component {...pageProps} key={router.route}/>
+      </Layout>
+    </AnimatePresence>
+  );
 }
 
-export default MyApp
+export default MyApp;
